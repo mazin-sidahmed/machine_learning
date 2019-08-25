6b37d16aa6c13e1e615d1bb61e483779fc88c213
mazin-sidahmed <mazinsidahmed@gmail.com>
2019-08-25 11:00:07 +0200

    scaling the data

--- mazin.py
+++ mazin.py
@@ -1,4 +1,5 @@
 import numpy as np 
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -15,6 +16,7 @@ X[:, 1:3] = imp_mean.transform(X[:, 1:3])
 print('Taking care of missing data')
 print (X[:])
 
+#categorical data
 from sklearn.preprocessing import LabelEncoder , OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[: , 0] = labelencoder_X.fit_transform(X[:, 0])
@@ -29,3 +31,12 @@ labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 print ('Depending variable')
 print (X)
+#split the data set into training and test set
+from sklearn.model_selection import train_test_split
+X_train,X_test , y_train, y_test = train_test_split(X,y , test_size = 0.2, random_state=0)
+print (X)
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler ()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+print (X_test)
